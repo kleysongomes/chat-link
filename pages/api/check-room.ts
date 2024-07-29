bfcbcc80b2f9d0404e6ab601744dc41e972f2e85
kleysongomes <kleysongomes7@gmail.com>
2024-07-29 13:06:56 -0300

    test: log

--- pages/api/check-room.ts
+++ pages/api/check-room.ts
@@ -1,22 +1,29 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Server } from 'socket.io';
+import { Server as HttpServer } from 'http';
+import { Socket as NetSocket } from 'net';
 
-let io: Server | null = null;
+type NextApiResponseWithSocket = NextApiResponse & {
+  socket: NetSocket & {
+    server: HttpServer & {
+      io?: Server;
+    };
+  };
+};
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+const checkRoomHandler = (req: NextApiRequest, res: NextApiResponseWithSocket) => {
   if (req.method === 'GET') {
-    if (!io) {
-      io = new Server({
-        cors: {
-          origin: '*',
-        },
-      });
-    }
-
     const { roomId } = req.query;
-    const roomExists = io.sockets.adapter.rooms.has(roomId as string);
-    res.status(200).json({ exists: roomExists });
+
+    if (res.socket.server.io) {
+      const roomExists = res.socket.server.io.sockets.adapter.rooms.has(roomId as string);
+      res.status(200).json({ exists: roomExists });
+    } else {
+      res.status(500).json({ error: 'Socket server not initialized' });
+    }
   } else {
     res.status(405).json({ error: 'Method not allowed' });
   }
 };
+
+export default checkRoomHandler;
